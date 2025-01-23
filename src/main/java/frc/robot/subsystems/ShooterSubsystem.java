7948305df2f99d69780a5fe57fcc93b06613dba6
msorola <101956921+msorola@users.noreply.github.com>
2025-01-23 17:40:06 -0600

    initial commit

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -6,38 +6,58 @@ package frc.robot.subsystems;
 
 import frc.robot.Constants;
 
-import com.revrobotics.AbsoluteEncoder;
-import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkAbsoluteEncoder;
+import com.revrobotics.AbsoluteEncoder;
+
+import com.revrobotics.spark.ClosedLoopSlot;
+
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.AbsoluteEncoderConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 
-import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+
+
+
+
+
 public class ShooterSubsystem extends SubsystemBase {
-  private SparkMax sparkMaxShooterMotor = new SparkMax(3, MotorType.kBrushless);
-  private SparkClosedLoopController pidController = sparkMaxShooterMotor.getClosedLoopController();
-  private SparkMaxConfig config = new SparkMaxConfig();
-  //private SparkMaxConfig.closedLoop.feedbackSensor canEncoder = sparkMaxShooterMotor.getAbsoluteEncoder();
-  
-  /** Creates a new ExampleSubsystem. */
+  private SparkMaxConfig config;
+  private SparkMax sparkMaxShooterMotor;
+  private SparkClosedLoopController pidController;
+  private AbsoluteEncoder encoder;
+
   public ShooterSubsystem() {
+  
+    sparkMaxShooterMotor = new SparkMax(3, MotorType.kBrushless);
+    pidController = sparkMaxShooterMotor.getClosedLoopController();
+    encoder = sparkMaxShooterMotor.getAbsoluteEncoder();
+
+  
+    config = new SparkMaxConfig();
+
+    config.absoluteEncoder
+        .positionConversionFactor(1);
+
     config.closedLoop
                     .pid(Constants.ShooterConstant.kP, Constants.ShooterConstant.kI , Constants.ShooterConstant.kD)
-                    .feedbackSensor(FeedbackSensor.kAbsoluteEncoder)
+                    .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
                     .maxOutput(Constants.ShooterConstant.kMaxOutput)
-                    .minOutput(Constants.ShooterConstant.kMinOutput);
+                    .minOutput(Constants.ShooterConstant.kMinOutput)
 
-      sparkMaxShooterMotor.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
+                    .p(0.0001, ClosedLoopSlot.kSlot1)
+                    .i(0,ClosedLoopSlot.kSlot1)
+                    .d(0,ClosedLoopSlot.kSlot1)
+                    .outputRange(-1,1, ClosedLoopSlot.kSlot1);
+
+    sparkMaxShooterMotor.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
       
   }
  
@@ -76,8 +96,8 @@ public class ShooterSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    //System.out.print("Encoder Position:");
-    //System.out.print(canEncoder.getPosition());
+    SmartDashboard.putNumber("Encoder Position:", encoder.getPosition());
+    SmartDashboard.putNumber("Velocity", encoder.getVelocity());
   }
 
   @Override
